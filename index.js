3d98ea5885b9c3d3fdddfc76c570ff84772e18ea
motti yudkin <motti4432@gmail.com>
2021-12-12 13:26:19 +0200

    update port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { join } = require("fs");
 const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, "build")));
 
@@ -9,6 +10,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(3001, () => {
-  console.log(`Example app listening at http://localhost:3001`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
